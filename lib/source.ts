1a4ae57e72afab1783df1ec4f9e9e7d1c402b9f1
erweixin <2991552132@qq.com>
2026-06-23 18:16:45 +0800

    chore: delete project

--- lib/source.ts
+++
@@ -1,9 +0,0 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
-import { i18n } from '@/lib/i18n';
- 
-export const source = loader({
-  i18n,
-  baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
-});
--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { createI18nMiddleware } from 'fumadocs-core/i18n';
-import { i18n } from '@/lib/i18n';
-
-export default createI18nMiddleware(i18n);
-
-export const config = {
-  // 排除根路径 `/`，只匹配其它路径
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|$|sitemap.xml|robots.txt).*)'],
-};
